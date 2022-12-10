92193415a5f77c43780c328bb60798bb67b8a55d
pepegc <jpgarcia90@gmail.com>
2022-12-10 09:40:04 -0500

    added data-sveltekit-reload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BUG REPRODUCTION
 
-Originally posted on Discord[https://discord.com/channels/457912077277855764/1050902033118527628], identified by @geoffrich[https://github.com/geoffrich] as a possible issue with the Vercel adapter.
+Originally posted on [Discord](https://discord.com/channels/457912077277855764/1050902033118527628), identified by [@geoffrich](https://github.com/geoffrich) as a possible issue with the Vercel adapter.
 
 Issue: TBD
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,30 +9,30 @@
 
 <div>
     TEST 1
-    <a href='/foo/bar/foobar'>/foo/bar/foobar</a>
+    <a data-sveltekit-reload href='/foo/bar/foobar'>/foo/bar/foobar</a>
 </div>
 
 <div>
     TEST 2
-    <a href='/foo/bar/foobar/foo'>/foo/bar/foobar/foo</a>
+    <a data-sveltekit-reload href='/foo/bar/foobar/foo'>/foo/bar/foobar/foo</a>
 </div>
 
 <div>
     TEST 3
-    <a href='/foo/index/bar'>/foo/index/bar</a>
+    <a data-sveltekit-reload href='/foo/index/bar'>/foo/index/bar</a>
 </div>
 
 <div>
     TEST 4
-    <a href='/foo/bar/index'>/foo/bar/index</a>
+    <a data-sveltekit-reload href='/foo/bar/index'>/foo/bar/index</a>
 </div>
 
 <div>
     TEST 5
-    <a href='/foo/bar/foobar/index'>/foo/bar/foobar/index</a>
+    <a data-sveltekit-reload href='/foo/bar/foobar/index'>/foo/bar/foobar/index</a>
 </div>
 
 <div>
     TEST 6
-    <a href='/foo/bar/foobar/index/foo'>/foo/bar/foobar/index/foo</a>
+    <a data-sveltekit-reload href='/foo/bar/foobar/index/foo'>/foo/bar/foobar/index/foo</a>
 </div>
